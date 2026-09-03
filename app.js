e173e211e0dd3fee336c7d749a25aadbae87e94e
FdilzDesign <iseffadilah7@gmail.com>
2026-09-03 21:33:44 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const firstname = 'Isef';
+const lastname = 'fadil';
+const favoritecolor = 'biru';
+const text = 'yo,namaku' + ' ' + firstname + ' ' + lastname + ' ' + 'warna kesukaanku' + ' ' + favoritecolor;
+
+console.log(text);
