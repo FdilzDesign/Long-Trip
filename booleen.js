ac66f83370eb7e8e5884a3ee335cb7ba04fcccae
FdilzDesign <iseffadilah7@gmail.com>
2026-09-08 15:00:12 +0700

    apa itu Booleen?

---
+++ booleen.js
@@ -0,0 +1,18 @@
+//Booleen adalah 2 variable  yaitu false,True
+
+const Anak = false;
+const Pelajar = true;
+const Pekerja = true;
+
+//biar lebih jelas pake kata kerja is,has,dll//
+
+const hasChild = false;
+const isStudent = true;
+const isEmploye = true;
+
+console.log('apakah kamu punya anak? ' + hasChild);
+console.log('apakah kamu adalah pelajar? ' + isStudent);
+console.log('apakah kamu adalah pekerja? ' + isEmploye);
+
+
+
