721f15210fe2da912039ee7eef62dfba990d6243
FdilzDesign <iseffadilah7@gmail.com>
2026-09-08 15:38:52 +0700

    Contoh Simple Pemakaian IF-Else & Conditioning

---
+++ Conditional.js
@@ -0,0 +1,9 @@
+const punyaDuit = false;
+if(punyaDuit) {
+    console.log("Bisa Top up Game")
+} else {
+    console.log("Ga Bisa Top up Game")
+};
+
+//Contoh Simple Pemakaian IF_ELse & Kondisi Sesuatu
+//Yg Tidak Sesuai Dgn Yg Kita Mau
