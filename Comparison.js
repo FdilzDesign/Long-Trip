5d37998051b294482e3f54c1ba92ae64af924648
FdilzDesign <iseffadilah7@gmail.com>
2026-09-08 16:18:09 +0700

    Contoh & Apa Saja Tipe Data Comparison Operators

---
+++ Comparison.js
@@ -0,0 +1,18 @@
+const nilai = 'abc' === 'abc'
+const nilai1 = false !== true
+const nilai2 ='cab' === 'cba'
+const nilai3 = false === false
+
+console.log(nilai);
+console.log(nilai1);
+console.log(nilai2);
+console.log(nilai3);
+
+//Comparison Biasa Digunakan Untuk Membandingkan 2 Nilai(value)
+//Contoh2 Tipe Comparison=
+//Sama dengan(===)
+//Tidak sama dengan(!==)
+//Lebih daro(>)
+//Kurang dari(<)
+//Lebih dari atau sama dengan(>=)
+//Kurang dari atau sama dengan(<=)
