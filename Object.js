4669285dbb7535cd3c2446c95ed14aca4c65cdec
FdilzDesign <iseffadilah7@gmail.com>
2026-09-11 23:56:52 +0700

    Pemakaian Object&Contoh

--- Object.js
+++ Object.js
@@ -4,11 +4,16 @@ const objek ={
     lahir : 2007,
     jobs : true,
 
-    a: [10,10,20,30]
+    a: [10,10,20,30],
+
+    sekolah : {
+        jurusan : 'TKJ',
+        Kelas : '12'
+    }
 }; 
 
 const text1 =objek.nama + ' lahir pada tahun ' + objek.lahir;
-console.log(objek.a[2]);
+console.log(text1);
 
 
 //objek.nama,umur dll
