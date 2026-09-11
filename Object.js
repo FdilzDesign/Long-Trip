8193a59daf6113585b7c28d33cc0615fefd024ee
FdilzDesign <iseffadilah7@gmail.com>
2026-09-11 23:52:02 +0700

    Pemakaian Object&Contoh

---
+++ Object.js
@@ -0,0 +1,15 @@
+const objek ={ 
+    nama : 'werr',
+    umur : 20,
+    lahir : 2007,
+    jobs : true,
+
+    a: [10,10,20,30]
+}; 
+
+const text1 =objek.nama + ' lahir pada tahun ' + objek.lahir;
+console.log(objek.a[2]);
+
+
+//objek.nama,umur dll
+//titik. bracket[] + bracket {}
